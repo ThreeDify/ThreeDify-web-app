ea3e9ab421e3dd0d520d36379ae21ace5a1b0ca0
Kishor Ghising <bhfty99@gmail.com>
2020-04-23 23:24:22 +0545

    Check lint error

--- src/Components/Icon.js
+++ src/Components/Icon.js
@@ -4,7 +4,7 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faUser, faEnvelope, faKey, faEye, faEyeSlash} from '@fortawesome/free-solid-svg-icons';
 
-library.add(faUser, faEnvelope, faKey, faEye, faEyeSlash)
+library.add(faUser, faEnvelope, faKey, faEye, faEyeSlash);
 
 export default class Icon extends React.Component{
   render() {
