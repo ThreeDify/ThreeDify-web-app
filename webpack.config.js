46c62cca0d7d1c730e709b2d68c1c957a625e448
silwalanish <silwalanish@gmail.com>
2020-04-24 20:17:02 +0545

    Extract css to separate file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
 const dotenv = require('dotenv');
+const webpack = require('webpack');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = () => {
-  const env = dotenv.config().parsed || { "API_URL": process.env.API_URL };
+  const env = dotenv.config().parsed || { 'API_URL': process.env.API_URL };
 
   const envKeys = Object.keys(env).reduce((prev, next) => {
     prev[`process.env.${next}`] = JSON.stringify(env[next]);
@@ -11,37 +14,61 @@ module.exports = () => {
   }, {});
 
   return {
-    entry: "./src/index.js",
-    mode: "development",
+    entry: './src/index.js',
+    mode: process.env.NODE_ENV || 'development',
     module: {
       rules: [
         {
           test: /\.(js|jsx)$/,
           exclude: /(node_modules|bower_components)/,
-          loader: "babel-loader",
-          options: { presets: ["@babel/env"] }
+          loader: 'babel-loader',
+          options: { presets: ['@babel/env'] }
         },
         {
           test: /\.css$/,
-          use: ["style-loader", "css-loader"]
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                hmr: process.env.NODE_ENV === 'development'
+              }
+            },
+            'css-loader'
+          ]
         }
       ]
     },
-    resolve: { extensions: ["*", ".js", ".jsx"] },
+    resolve: { extensions: ['*', '.js', '.jsx'] },
     output: {
-      path: path.resolve(__dirname, "dist/"),
-      publicPath: "/dist/",
-      filename: "bundle.js"
+      path: path.resolve(__dirname, 'dist/'),
+      publicPath: '/dist/',
+      filename: 'bundle.js'
     },
     devServer: {
-      contentBase: path.join(__dirname, "public/"),
+      contentBase: path.join(__dirname, 'public/'),
       port: 3000,
-      publicPath: "http://localhost:3000/dist/",
+      publicPath: 'http://localhost:3000/dist/',
       hotOnly: true
+    },
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          styles: {
+            name: 'styles',
+            test: /\.css$/,
+            chunks: 'all',
+            enforce: true,
+          },
+        },
+      },
     },
     plugins: [
       new webpack.HotModuleReplacementPlugin(),
-      new webpack.DefinePlugin(envKeys)
+      new webpack.DefinePlugin(envKeys),
+      new OptimizeCSSAssetsPlugin(),
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+      })
     ]
   };
 };
