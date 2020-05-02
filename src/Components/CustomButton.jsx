eb868a8c295b8647597df096e11b85085f282274
silwalanish <silwalanish@gmail.com>
2020-05-02 09:59:56 +0545

    Add CustomButton component

--- src/Components/CustomButton.jsx
+++ src/Components/CustomButton.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
-import '../Themes/CustomButton.css'; 
-export default class Button extends React.Component{
+import PropTypes from 'prop-types';
+
+import '../Themes/CustomButton.css';
+
+const DEFAULT_BTN_TYPE = 'button';
+
+class CustomButton extends React.Component {
   render() {
-    return(
-      <button onClick="" className="button" type={this.props.type}>{this.props.name}</button>
+    return (
+      <button
+        onClick={this.props.onClickHandle}
+        className='button'
+        type={this.props.type || DEFAULT_BTN_TYPE}
+        disabled={this.props.isDisabled}
+      >
+        {this.props.children}
+      </button>
     );
-  }         
+  }
 }
+
+CustomButton.propTypes = {
+  type: PropTypes.string,
+  isDisabled: PropTypes.bool,
+  onClickHandle: PropTypes.func,
+  children: PropTypes.node.isRequired,
+};
+
+export default CustomButton;
