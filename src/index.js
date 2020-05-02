0d73b3b02d1b04c00580281ef277bcb396dd060e
silwalanish <silwalanish@gmail.com>
2020-05-02 19:05:29 +0545

    Install bootstrap

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
+import 'bootstrap';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+
 import App from './App.jsx';
 import store from './Store/index';
+import ICONS from './iconSettings';
+
+library.add(ICONS);
 
 ReactDOM.render(
   <Provider store={store}>
