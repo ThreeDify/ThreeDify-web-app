c64e2367427a2114025c2a17c48f04036cefff37
silwalanish <silwalanish@gmail.com>
2020-05-18 11:53:46 +0545

    Update the login api url

--- src/Constants/apiUrls.js
+++ src/Constants/apiUrls.js
@@ -4,4 +4,4 @@
 
 export const LOGIN_URL = process.env.API_URL + '/api/auth/jwt/create';
 
-export const USER_INFO_URL = process.env.API_URL + '/api/auth/users/me';
+export const USER_INFO_URL = process.env.API_URL + '/api/users/me';
