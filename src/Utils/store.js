1e7f43035f006f46c6f2df4d55f5a63eb9b02e03
silwalanish <silwalanish@gmail.com>
2020-05-22 13:37:48 +0545

    Only store userToken in localStorage

--- src/Utils/store.js
+++ src/Utils/store.js
@@ -1,8 +1,17 @@
 const STORAGE_KEY = 'state';
 
+function getStatesToSave(state) {
+  return {
+    auth: {
+      userToken: state.auth.userToken,
+      isLoggedIn: state.auth.isLoggedIn,
+    },
+  };
+}
+
 export function persistState(state) {
   try {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(state));
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(getStatesToSave(state)));
   } catch (e) {
     console.log(e);
   }
