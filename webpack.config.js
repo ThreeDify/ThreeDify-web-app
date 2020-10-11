6b3cc799b338dfec314f290dfdb2e9b32134e8cf
silwalanish <silwalanish@gmail.com>
2020-10-11 13:19:29 +0545

    Fix configuration for babel

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = () => {
           test: /\.(js|jsx)$/,
           exclude: /(node_modules|bower_components)/,
           loader: 'babel-loader',
-          options: { presets: ['@babel/env'] },
+          options: require('./babel.config.json'),
         },
         {
           test: /\.s?css$/,
