36ef86a7408a034c03161e27bb9422711547bc73
danny237 <dannymagar37@gmail.com>
2020-11-05 14:50:35 +0545

    api integration complete in upload form

--- src/Pages/Reconstruction.jsx
+++ src/Pages/Reconstruction.jsx
@@ -64,7 +64,7 @@ export class Reconstruction extends Component {
     this.clickHandler = this.clickHandler.bind(this);
   }
 
-  clickHandler(e) {
+  clickHandler() {
     this.form.current.reset();
     this.setState({
       uploading: false,
