858f694ef00ec931fa8204dffac3c0a16084a388
Kishor Ghising <bhfty99@gmail.com>
2020-11-10 01:51:56 +0545

    Implement team member card

---
+++ src/Components/TeamMemberCard.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Icon from '../Components/Icon';
+
+class TeamMemberCard extends React.Component {
+  render() {
+    return (
+      <div className='team-member-card col-xl-3 col-lg-4 col-md-6 col-sm-12'>
+        <div className='member-img'>
+          <img src={this.props.imgLink} />
+        </div>
+        <h5>{this.props.name}</h5>
+        <p>{this.props.description}</p>
+        <div className='social-media'>
+          <a target='_blank' rel='noreferrer' href={this.props.githubLink}>
+            <Icon
+              className='social-media-icon'
+              name={['fab', 'github']}
+              size='2x'
+            />
+          </a>
+          <a target='_blank' rel='noreferrer' href={this.props.instagramLink}>
+            <Icon
+              className='social-media-icon'
+              name={['fab', 'instagram']}
+              size='2x'
+            />
+          </a>
+          <a target='_blank' rel='noreferrer' href={this.props.linkedinLink}>
+            <Icon
+              className='social-media-icon'
+              name={['fab', 'linkedin']}
+              size='2x'
+            />
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+TeamMemberCard.propTypes = {
+  imgLink: PropTypes.string,
+  name: PropTypes.string,
+  description: PropTypes.string,
+  githubLink: PropTypes.string,
+  instagramLink: PropTypes.string,
+  linkedinLink: PropTypes.string,
+};
+
+export default TeamMemberCard;
